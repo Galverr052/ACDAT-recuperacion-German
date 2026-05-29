01c3086d9e2b91bac612c8d86c5de6cdd86a7409
Galverr052 <galverr052@g.educaand.es>
2026-05-29 14:21:56 +0200

    ejercicios de examen

--- ad_recuperacion/src/main/java/iesmm/ad/ad_recuperacion/controller/ws1.java
+++ ad_recuperacion/src/main/java/iesmm/ad/ad_recuperacion/controller/ws1.java
@@ -1,9 +1,7 @@
 package iesmm.ad.ad_recuperacion.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
@@ -12,7 +10,7 @@ import java.time.format.DateTimeFormatter;
 @RequestMapping("/ws")
 public class ws1 {
 
-    @GetMapping("/ex/hora")
+    @PostMapping("/ex/hora")
     @ResponseBody
     public String hora (){
         LocalTime hora = LocalTime.now();
@@ -21,5 +19,63 @@ public class ws1 {
     }
 
 
+    @GetMapping("ex/numeros")
+    @ResponseBody
+    public String numeros(@RequestParam int min, @RequestParam int max){
+
+        int [] numeros = new int[max - min + 1];
+        int n = 0;
+
+        for (int i = min ; i < max + 1; i++) {
+            numeros[n] = i;
+            n++;
+        }
+        String resultado = "[ ";
+
+        int x = 0;
+
+        for (int numero : numeros) {
+            resultado += numero;
+
+            if (x < numeros.length - 1) {
+                resultado += ",";
+            }
+
+            x++;
+        }
+
+        return resultado + "]";
+    }
+
+    @GetMapping(value = "ex/nveces",  produces = "application/xml")
+    @ResponseBody
+    public String frase(@RequestParam String frase) {
+
+        StringBuilder xml = new StringBuilder();
+        xml.append("<lista>");
+
+        String Procesados = "";
+
+        for (char c : frase.toCharArray()) {
+
+            if (Procesados.indexOf(c) == -1) {
+                int count = 0;
+
+                for (char x : frase.toCharArray()) {
+                    if (x == c) count++;
+                }
+
+                xml.append("<caracter>");
+                xml.append("<simbolo>").append(c).append("</simbolo>");
+                xml.append("<nveces>").append(count).append("</nveces>");
+                xml.append("</caracter>");
+
+                Procesados += c;
+            }
+        }
+
+        xml.append("</lista>");
+        return xml.toString();
+    }
 
 }
